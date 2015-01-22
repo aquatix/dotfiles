19fc80d4185cb05db700f0c8f8249c492ec7ed6a
Michiel Scholten <michiel@aquariusoft.org>
2015-01-22 09:18:58 +0100

    20101214: log hdd temps, initial version

---
+++ bin/cron/temperature_hdd_log.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#temperature:             23 C
+
+#TEMPERATURE=`awk '{print $2}' /proc/acpi/thermal_zone/THRM/temperature`
+MONTH=`date +%Y-%m`
+DATE=`date +%Y-%m-%d`
+for HDD in sda sdb sdc
+do
+	RAWTEMP=`/usr/sbin/hddtemp /dev/$HDD`
+	#echo $RAWTEMP
+	#TEMPERATURE=`awk '{print $NF}' /root/tmp/hddtemp`
+	TEMPERATURE=`echo $RAWTEMP | awk '{print $NF}' | sed 's/°C//g'`
+	LOG="/var/log/custom_logs/hdd/temperature_${HDD}_${MONTH}.log"
+	TIMESTAMP=`date "+%Y%m%d %H%M"`
+	echo "${TIMESTAMP} $TEMPERATURE" >> ${LOG}
+	#echo "${TIMESTAMP} $TEMPERATURE"
+
+	LOG="/var/log/custom_logs/temp_daily/hdd/temperature_${HDD}_${DATE}.log"
+	#TIMESTAMP=`date "+%Y%m%d %H%M"`
+	echo "${TIMESTAMP} $TEMPERATURE" >> ${LOG}
+done
