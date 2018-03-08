a65226e568eb6780f89ad0489fa224ab66f9f3d8
Michiel Scholten <michiel@diginaut.net>
2018-03-08 22:42:56 +0100

    Let's try ripgrep instead of silver searcher for FZF

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -40,7 +40,8 @@ end
 set -x QT_AUTO_SCREEN_SCALE_FACTOR 1
 
 # SilverSearcher 'ag' ('ack' and 'grep' replacement)
-set -gx FZF_DEFAULT_COMMAND 'ag -g ""'
+#set -gx FZF_DEFAULT_COMMAND 'ag -g ""'
+set -gx FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 set -gx FZF_ALT_C_COMMAND "$FZF_DEFAULT_COMMAND"
 
