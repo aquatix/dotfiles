2e7fa8ac98a29af5940b2d51f3bbc87a85a85432
Michiel Scholten <michiel@diginaut.net>
2018-07-23 11:32:50 +0200

    Add user base bin dir, for use with pipenv and such

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,6 +18,10 @@ set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 set -U grcplugin_ls --color
 
 # PATH
+## If the 'user base' binary directory exists, add it to PATH
+if test -x ~/.local/bin
+    set PATH $PATH ~/.local/bin
+end
 if test -x ~/.dot/dotfiles/bin
     set PATH $PATH ~/.dot/dotfiles/bin
 end
