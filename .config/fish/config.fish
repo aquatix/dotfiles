18b9e5c8ec18f0af3445a8764a6a13f1113317dc
Michiel Scholten <michiel@diginaut.net>
2021-07-08 10:32:17 +0200

    Current fisher cannot be installed automatically

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,12 +3,11 @@ set EDITOR vim
 
 set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
 
-if not functions -q fisher
-    echo "Installing fisher for the first time..." >&2
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
+#if not functions -q fisher
+#    DO NOT ENABLE, it is a fork bomb :)
+#    echo "Installing fisher for the first time..." >&2
+#    curl -sL https://git.io/fisher | source && fisher update
+#end
 
 # Theme options for bobthefish
 set -g theme_nerd_fonts yes
